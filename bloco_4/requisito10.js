e68663448cb7dba8d5cc337070ddb426be34eb9c
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-25 12:18:40 -0300

    Executado requisito 10

---
+++ bloco_4/requisito10.js
@@ -0,0 +1,7 @@
+let custoProduto = 50;
+let impostoCusto = custoProduto * 0.2;
+let vendaProduto = 80;
+let custoTotal = custoProduto + impostoCusto;
+let lucro = vendaProduto - custoTotal;
+
+console.log(lucro * 1000);
