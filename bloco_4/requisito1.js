bbae164815cdd0293734c7ae05c90739dc657b86
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-25 07:55:20 -0300

    requisito 1 executado

---
+++ bloco_4/requisito1.js
@@ -0,0 +1,8 @@
+let numero1 = 3;
+let numero2 = 5;
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+console.log(numero1 ** numero2);
