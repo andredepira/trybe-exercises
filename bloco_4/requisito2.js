49758b80d016826d633f58ecfd50267040dfd151
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-25 08:03:33 -0300

    Executado o requisito 2

---
+++ bloco_4/requisito2.js
@@ -0,0 +1,8 @@
+let numero1 = 10;
+let numero2 = 5;
+
+if (numero1 > numero2) {
+    console.log("O numero 1 é maior que o numero 2");
+} else {
+    console.log("O numero 2 é maior que o numero 1");
+}
