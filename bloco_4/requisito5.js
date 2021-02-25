0a70f9c90cfae4cf2e8aa1cea64fe7c35796abe6
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-25 11:08:54 -0300

    Executado requisito 5

---
+++ bloco_4/requisito5.js
@@ -0,0 +1,10 @@
+let angulo1 = 80;
+let angulo2 = 70;
+let angulo3 = 30;
+let somaAngulos = angulo1 + angulo2 + angulo3;
+
+if (somaAngulos === 180) {
+    console.log(true);
+} else {
+    console.log(false);
+}
