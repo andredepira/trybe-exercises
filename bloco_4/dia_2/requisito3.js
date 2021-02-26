30500475cf7b4bb3ef2d42d0b5d9eb8f7e590976
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-26 11:36:49 -0300

    executado requisito 3

--- bloco_4/dia_2/requisito3.js
+++ bloco_4/dia_2/requisito3.js
@@ -1,1 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let indexSoma = 0; indexSoma < numbers.length; indexSoma += 1) {
+    soma += numbers[indexSoma];
+}
+
+console.log("A média é " + soma / numbers.length);
