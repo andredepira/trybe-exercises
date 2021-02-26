049e5c64dd4143a08a712b8f53da541e3020243e
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-26 12:00:29 -0300

    executado requisito 4

--- bloco_4/dia_2/requisito4.js
+++ bloco_4/dia_2/requisito4.js
@@ -1,1 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    soma += numbers[indexNumbers];
+}
+
+if (soma / numbers.length > 20){
+        console.log("O valor da média é maior que 20")
+    } else {
+        console.log("O valor da média é menor que 20")
+}
+
