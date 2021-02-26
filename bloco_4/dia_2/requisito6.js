5ef301bd5339c5697ae790eedf33bb4755e28f6a
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-26 13:11:07 -0300

    executado requisito 6

--- bloco_4/dia_2/requisito6.js
+++ bloco_4/dia_2/requisito6.js
@@ -1,1 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddNumbers = 0;
+
+for (let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    if (numbers[indexNumbers] % 2 !== 0) {
+        oddNumbers += 1  
+    } 
+}
+
+if (oddNumbers === 0) {
+    console.log("Nenhum número ímpar foi encontrado");
+} else {
+    console.log(oddNumbers);
+}
