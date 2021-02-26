13df72119af5898a90b2c0af6114187a0336f93e
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-26 12:45:34 -0300

    executado requisito 5

--- bloco_4/dia_2/requisito5.js
+++ bloco_4/dia_2/requisito5.js
@@ -1,1 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0;
+
+for (let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    if (numbers[indexNumbers] > higherNumber){
+        higherNumber = numbers[indexNumbers];
+    }
+}
+
+console.log(higherNumber);
