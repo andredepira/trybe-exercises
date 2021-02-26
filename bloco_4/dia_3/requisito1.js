2939be220e984e635e2311887c36651d97071d8a
André Arnoni <andre_arnoni@yahoo.com.br>
2021-02-26 18:36:11 -0300

    Executado o requisito 1

--- bloco_4/dia_3/requisito1.js
+++ bloco_4/dia_3/requisito1.js
@@ -1,1 +1,11 @@
-let n = 10;
+let n = 5;
+let character = "*";
+let lineCharacter = "";
+
+for (let indexN = 0; indexN < n; indexN += 1) {
+    lineCharacter = lineCharacter + character;
+}
+
+for (let indexN = 0; indexN < n; indexN += 1) {
+    console.log(lineCharacter);
+}
