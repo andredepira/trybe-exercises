77eab21d9458e75e1e4b8341ef1a3394b55064eb
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-04 19:34:45 -0300

    Adicionando exercicios

--- bloco_4/dia_4/parte2/requisito5.js
+++ bloco_4/dia_4/parte2/requisito5.js
@@ -1,4 +1,3 @@
-//Duas formas de resolver o exercício
 function maisRepetido(numeros) {
     let contRepetido = 0;
     let contNumero = 0;
@@ -17,31 +16,6 @@ function maisRepetido(numeros) {
       contNumero = 0;
     }
     return numeros[indexNumeroRepetido];
-  }
-  
-  function maisRepetido(numeros) {
-    let num = {};
-  
-    for (let i = 0; i < numeros.length; i += 1) {
-      let valor = numeros[i];
-      if (num[valor] === undefined) {
-        num[valor] = 1;
-      } else {
-        num[valor] = num[valor] + 1;
-      }
-    }
-  
-    let contRepetido = 0;
-    let contNumero = 0;
-  
-    for (let prop in num) {
-      if (contRepetido < num[prop]) {
-        contRepetido = num[prop];
-        contNumero = prop;
-      }
-    }
-  
-    return contNumero;
   }
   
   console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
